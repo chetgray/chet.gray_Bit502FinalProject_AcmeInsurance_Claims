ffc6cb57f413a7b7f2f7e77c0c888d20f7555aae
Chet Gray <chet.gray@waystar.com>
2023-06-22 15:19:48 -0400

    🗃️ feat(Database): define tables

---
+++ AcmeInsurance.Claims.Database/Tables/Provider.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE [dbo].[Provider] (
+    [Id]          INT           NOT NULL IDENTITY
+  , [Code]        NVARCHAR(850) NOT NULL
+  , [Name]        NVARCHAR(850) NOT NULL
+  , [IsInNetwork] BIT           NOT NULL
+  , [IsPreferred] BIT           NOT NULL
+
+  , CONSTRAINT [PK_Provider_Id]   PRIMARY KEY ([Id])
+  , CONSTRAINT [UQ_Provider_Code] UNIQUE      ([Code])
+)
+;
+
+GO
+
