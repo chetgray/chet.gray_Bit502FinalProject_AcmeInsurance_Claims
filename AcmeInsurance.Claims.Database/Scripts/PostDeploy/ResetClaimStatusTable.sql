2f2a65912655fb6c4527bcf8fd1f9a3ec29f50c6
Chet Gray <chet.gray@waystar.com>
2023-06-23 12:03:12 -0400

    🗃️ build(Database): seed data in post-deploy scripts

---
+++ AcmeInsurance.Claims.Database/Scripts/PostDeploy/ResetClaimStatusTable.sql
@@ -0,0 +1,27 @@
+﻿SET IDENTITY_INSERT [ClaimStatus] ON;
+
+-- 1, pending
+-- 2, approved
+-- 3, denied
+
+MERGE INTO [ClaimStatus] AS [tgt]
+USING (
+              SELECT 1,  'pending'
+    UNION ALL SELECT 2, 'approved'
+    UNION ALL SELECT 3,   'denied'
+) AS [src]       ([Id],    [Value])
+ON
+    [tgt].[Id] = [src].[Id]
+WHEN MATCHED THEN
+    UPDATE SET
+    [tgt].[Value] = [src].[Value]
+WHEN NOT MATCHED BY TARGET THEN
+    INSERT (      [Id],       [Value])
+    VALUES ([src].[Id], [src].[Value])
+WHEN NOT MATCHED BY SOURCE THEN
+    DELETE
+;
+
+SET IDENTITY_INSERT [ClaimStatus] OFF;
+
+GO
