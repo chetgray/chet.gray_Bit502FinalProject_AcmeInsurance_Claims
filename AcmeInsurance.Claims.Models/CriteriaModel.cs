1e606832ad44ffc44c52bb319be97b38d681adde
Chet Gray <chet.gray@waystar.com>
2023-06-28 11:48:08 -0400

    ✨ feat(Models): implement `CriteriaModel`

---
+++ AcmeInsurance.Claims.Models/CriteriaModel.cs
@@ -0,0 +1,12 @@
+﻿namespace AcmeInsurance.Claims.Models
+{
+    public class CriteriaModel : ICriteriaModel
+    {
+        public int Id { get; set; }
+
+        public decimal DenialMinimumAmount { get; set; }
+        public bool RequiresProviderIsInNetwork { get; set; }
+        public bool RequiresProviderIsPreferred { get; set; }
+        public bool RequiresClaimHasPreApproval { get; set; }
+    }
+}
