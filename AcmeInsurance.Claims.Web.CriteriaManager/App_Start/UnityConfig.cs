7729d09ce52d2c9de12b9de85224a075f12d726e
Chet Gray <chet.gray@waystar.com>
2023-06-28 05:35:56 -0400

    🎉 chore: set up N-tier projects and Unity config

---
+++ AcmeInsurance.Claims.Web.CriteriaManager/App_Start/UnityConfig.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+using Unity;
+
+namespace AcmeInsurance.Claims.Web.CriteriaManager
+{
+    internal static class UnityConfig
+    {
+        private static readonly Lazy<IUnityContainer> _container =
+            new Lazy<IUnityContainer>(() =>
+            {
+                IUnityContainer container = new UnityContainer();
+                RegisterTypes(container);
+
+                return container;
+            });
+
+        public static IUnityContainer GetConfiguredContainer()
+        {
+            return _container.Value;
+        }
+
+        private static void RegisterTypes(IUnityContainer container) { }
+    }
+}
