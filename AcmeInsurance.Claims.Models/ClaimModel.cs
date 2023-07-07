3317242f987dadad88d0934ebe051e4490331433
Chet Gray <chet.gray@waystar.com>
2023-07-07 10:50:05 -0400

    ✨ feat(Models): implement `ClaimModel` and `ProviderModel`

---
+++ AcmeInsurance.Claims.Models/ClaimModel.cs
@@ -0,0 +1,30 @@
+﻿using System.Runtime.Serialization;
+
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
+namespace AcmeInsurance.Claims.Models
+{
+    [JsonConverter(typeof(StringEnumConverter))]
+    public enum ClaimStatus
+    {
+        [EnumMember(Value = "pending")]
+        Pending = 1,
+
+        [EnumMember(Value = "approved")]
+        Approved = 2,
+
+        [EnumMember(Value = "denied")]
+        Denied = 3
+    }
+
+    public class ClaimModel : IClaimModel
+    {
+        public int Id { get; set; }
+        public string PatientName { get; set; }
+        public IProviderModel Provider { get; set; }
+        public decimal Amount { get; set; }
+        public bool HasPreApproval { get; set; }
+        public ClaimStatus ClaimStatus { get; set; }
+    }
+}
---
+++ AcmeInsurance.Claims.Models/IClaimModel.cs
@@ -0,0 +1,12 @@
+﻿namespace AcmeInsurance.Claims.Models
+{
+    public interface IClaimModel
+    {
+        int Id { get; set; }
+        string PatientName { get; set; }
+        IProviderModel Provider { get; set; }
+        decimal Amount { get; set; }
+        bool HasPreApproval { get; set; }
+        ClaimStatus ClaimStatus { get; set; }
+    }
+}
