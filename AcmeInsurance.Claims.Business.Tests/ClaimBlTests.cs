20076f44d7ff9c0a812c7a93c438f55028549262
Chet Gray <chet.gray@waystar.com>
2023-07-18 05:07:23 -0400

    ✅ test(Business): `GetClaimStatus` returns expected status

---
+++ AcmeInsurance.Claims.Business.Tests/ClaimBlTests.cs
@@ -0,0 +1,38 @@
+﻿using AcmeInsurance.Claims.Business.Tests.TestDoubles;
+using AcmeInsurance.Claims.Data;
+using AcmeInsurance.Claims.Data.Objects;
+using AcmeInsurance.Claims.Models;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using Unity;
+
+namespace AcmeInsurance.Claims.Business.Tests
+{
+    [TestClass]
+    public class ClaimBlTests
+    {
+        [TestMethod]
+        [DataRow(ClaimStatus.Pending)]
+        [DataRow(ClaimStatus.Approved)]
+        [DataRow(ClaimStatus.Denied)]
+        public void GetClaimStatus_ClaimExists_ReturnsClaimStatus(ClaimStatus expected)
+        {
+            // Arrange
+            const int id = 1;
+            UnityConfig.Container.RegisterInstance<IClaimRepository>(
+                new ClaimRepositoryStub
+                {
+                    ClaimDto = new ClaimDto { Id = id, ClaimStatusId = (int)expected }
+                }
+            );
+            IClaimBl bl = UnityConfig.Container.Resolve<IClaimBl>();
+
+            // Act
+            ClaimStatus actual = bl.GetClaimStatus(id);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ AcmeInsurance.Claims.Business.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("AcmeInsurance.Claims.Business.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Waystar")]
+[assembly: AssemblyProduct("AcmeInsurance.Claims.Business.Tests")]
+[assembly: AssemblyCopyright("Copyright © Waystar 2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("2a974ea5-5ac5-4191-9da6-ffcca030b99c")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
